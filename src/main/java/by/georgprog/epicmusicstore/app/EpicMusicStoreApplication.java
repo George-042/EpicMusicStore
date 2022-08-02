fa97065cbbde0b40c4a93bcb345b452a7bac727f
George-042 <thehealth02@gmail.com>
2022-08-02 22:15:09 +0300

    Creating application

---
+++ src/main/java/by/georgprog/epicmusicstore/app/EpicMusicStoreApplication.java
@@ -0,0 +1,12 @@
+package by.georgprog.epicmusicstore.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicMusicStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpicMusicStoreApplication.class, args);
+    }
+}
